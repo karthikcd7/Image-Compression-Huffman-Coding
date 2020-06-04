a384eb3b400bc21e22f27d002a18c2a0105ceb72
Karthik <42541700+karthikcd7@users.noreply.github.com>
2020-06-05 00:31:19 +0530

    Update imgtotxt.py

--- imgtotxt.py
+++ imgtotxt.py
@@ -1,13 +1,22 @@
 def imgtotxt(s):
 	from PIL import Image
 	from collections import Counter
-	im=Image.open(s).convert('L')
+	im=Image.open(s)
 	data=list(im.getdata())
-	counts=Counter(data)
-	l=[str(i)+'\n' for i in data]
-	l.append(str(im.size))
-	open('test1.txt','w').writelines(l)
-	f=open('test2.txt','w')
-	data=list(set(data))
+	open('test1.txt','w')
 	for i in data:
-		f.write(str(i)+','+str(counts[i])+'\n')
+		for j in range(3):
+			open('test1.txt','a').writelines(str(i[j]))
+			open('test1.txt','a').writelines('\n')
+	f=open('test2.txt','w')
+	with open('test1.txt', 'r') as fd:
+		lines = fd.read().split()
+		counter = Counter(lines)
+		# sorts items
+		items = sorted(counter.items(), key=lambda x: int(x[0]))
+		# prints desired output
+		for k, repetitions in items:
+			f.write(str(k)+','+str(repetitions)+'\n')
+	
+	fd=open('test1.txt','a')
+	fd.write(str(im.size))
