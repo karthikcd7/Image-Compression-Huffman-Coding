79f6309bd6b097585e0a691de11897e2dce8121e
Karthik <42541700+karthikcd7@users.noreply.github.com>
2020-06-05 00:32:21 +0530

    Update txttoimg.py

--- txttoimg.py
+++ txttoimg.py
@@ -2,11 +2,11 @@ def txttoimg(s):
 	from PIL import Image
 	f=open(s,'r').read()
 	f=f.split('\n')
-	l=[int(i) for i in f[:len(f)-1]]
+	l=[eval(i) for i in f[:len(f)-1]]
 	f=f[-1:][0]
 	f=f[1:len(f)-1]
-	size=f.split(', ')
-	size=tuple([int(i) for i in size])
-	im=Image.new('L',size)
+	size=f.split(',')
+	size=tuple(int(i) for i in size)
+	im=Image.new('RGB',size)
 	im.putdata(l)
 	im.save('pic.jpg')
